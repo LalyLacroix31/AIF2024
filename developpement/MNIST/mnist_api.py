a56f045fb8a8c9bbd9566499163069034234c17f
LalyLacroix31 <lalacroi@insa-toulouse.fr>
2023-10-09 16:05:16 +0200

    TP1-fichiers_modifiés

--- developpement/MNIST/mnist_api.py
+++ developpement/MNIST/mnist_api.py
@@ -9,10 +9,10 @@ from model import MNISTNet
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 app = Flask(__name__)
-
-parser = ... 
-... # add an argument '--model_path'
-model_path = ...
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_path', type=str, default = 'weights/mnist_net.pth', help='model path')
+args = parser.parse_args()
+model_path = args.model_path
 
 model = MNISTNet().to(device)
 # Load the model
@@ -41,5 +41,31 @@ def predict():
 
     return jsonify({"prediction": int(predicted[0])})
 
+@app.route('/batch_predict', methods=['POST'])
+def batch_predict():
+    # Get the image data from the request
+    images_binary = request.files.getlist("images[]")
+
+    tensors = []
+
+    for img_binary in images_binary:
+        img_pil = Image.open(img_binary.stream)
+        tensor = transform(img_pil)
+        tensors.append(tensor)
+
+    # Stack tensors to form a batch tensor
+    batch_tensor = torch.stack(tensors, dim=0)
+
+    # Make prediction
+    with torch.no_grad():
+        outputs = model(batch_tensor.to(device))
+        _, predictions = outputs.max(1)
+
+    return jsonify({"predictions": predictions.tolist()})
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+# Cette modification ajoute une nouvelle route /batch_predict qui accepte une liste d'images téléchargées via une requête POST. 
+# Les images sont lues à partir de la demande sous forme de fichiers, transformées et prédites individuellement, 
+# puis les prédictions sont renvoyées sous forme de liste JSON. 
